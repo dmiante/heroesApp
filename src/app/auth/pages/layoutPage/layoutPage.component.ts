ce9bc781ca22f840c7833c77538ba78a18590f52
dmiante <dmi4ndev@gmail.com>
2023-11-22 15:13:51 -0300

    ADD pages auth

---
+++ src/app/auth/pages/layoutPage/layoutPage.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-page',
+  templateUrl: 'layoutPage.component.html',
+  styleUrls: ['./layoutPage.component.css'],
+})
+export class LayoutPageComponent { }
