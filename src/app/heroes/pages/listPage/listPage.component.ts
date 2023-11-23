c2c50f5acadcf8a444da6f6dc9b303183e47de6c
dmiante <dmi4ndev@gmail.com>
2023-11-23 19:50:29 -0300

    ADD list of heroes from services

--- src/app/heroes/pages/listPage/listPage.component.ts
+++ src/app/heroes/pages/listPage/listPage.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { HeroesService } from './../../services/heroes.service';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-list-page',
   templateUrl: './listPage.component.html',
   styleUrls: ['./listPage.component.css'],
 })
-export class ListPageComponent { }
+export class ListPageComponent implements OnInit {
+  public heroes: Hero[] = []
+
+  constructor(private HeroesService: HeroesService) {}
+  ngOnInit(): void {
+    this.HeroesService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes)
+  }
+}
